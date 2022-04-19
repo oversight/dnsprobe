642853def58b38aa044b919c0c53fff7dbf60361
AnjaBruls <anja@transceptor.technology>
2022-04-19 13:22:19 +0200

    update __init__

--- lib/check/__init__.py
+++ lib/check/__init__.py
@@ -1,5 +1,28 @@
-from .checkDNS import CheckDNS
+from .checkA import CheckA
+from .checkAAAA import CheckAAAA
+from .checkCAA import CheckCAA
+from .checkCNAME import CheckCNAME
+from .checkDNSKEY import CheckDNSKEY
+from .checkDS import CheckDS
+from .checkMX import CheckMX
+from .checkNS import CheckNS
+from .checkPTR import CheckPTR
+from .checkSOA import CheckSOA
+from .checkSRV import CheckSRV
+from .checkTXT import CheckTXT
+
 
 CHECKS = {
-    'CheckDNS': CheckDNS
+    'CheckA': CheckA,
+    'CheckAAAA': CheckAAAA,
+    'CheckCAA': CheckCAA,
+    'CheckCNAME': CheckCNAME,
+    'CheckDNSKEY': CheckDNSKEY,
+    'CheckDS': CheckDS,
+    'CheckMX': CheckMX,
+    'CheckNS': CheckNS,
+    'CheckPTR': CheckPTR,
+    'CheckSOA': CheckSOA,
+    'CheckSRV': CheckSRV,
+    'CheckTXT': CheckTXT
 }
